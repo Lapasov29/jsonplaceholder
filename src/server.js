73e0d7939b92a63a2d033ad72a74e8ecffa0d6c1
Anvar Lapasov <lapasovanvar2908@gmail.com>
2021-12-22 10:02:26 +0300

    added api address

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 const app = express()
+const os = require('os');
+const api = os.networkInterfaces()[Object.keys(os.networkInterfaces())[0]][1].address
 const PORT = process.env.PORT || 3000
 let data = fs.readFileSync('./src/database/posts.json', 'utf-8')
 let comments = fs.readFileSync('./src/database/comments.json', 'utf-8')
@@ -17,12 +19,15 @@ app.get("/posts", (req, res) => {
     )
 })
 app.get('/posts/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(data[0].id, req.params.id);
+    console.log(data[0].id == req.params.id);
     res.json(
         data.find(post => post.id == req.params.id)
     )
 })
 
-app.get('/posts/:id/comment', (req, res) => {
+app.get('/posts/:id/comments', (req, res) => {
     res.json(
         comments.filter(com => com.postId == req.params.id)
     )
@@ -91,4 +96,4 @@ app.delete('/posts/:id', (req, res) => {
     fs.writeFileSync('./src/database/posts.json', JSON.stringify(data, null, 4))
 })
 
-app.listen(PORT, () => console.log('Server is running on http://192.168.1.253:' + PORT))
+app.listen(PORT, () => console.log(`Server is running on http://${api}:${PORT}`))
